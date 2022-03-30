ea3ae7296210841c8b7ccd3a502b081ffbacf6d6
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-03-30 12:29:06 -0400

    altera urls.py

--- fruitlibrary/urls.py
+++ fruitlibrary/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import url, include
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
